1b7fe29b47e43ede02e0bb961fcf6f1a683cc27c
ercanaytekin24-lang <ercanaytekin24@gmail.com>
2025-09-21 15:11:21 +0300

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,3 +1,16 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Bot çalışıyor!" });
+  if (req.method === "POST") {
+    const chatId = req.body.message.chat.id;
+    const text = req.body.message.text;
+
+    // Şimdilik sadece geri cevap veriyoruz (echo bot)
+    res.status(200).json({
+      method: "sendMessage",
+      chat_id: chatId,
+      text: "Sen yazdın: " + text
+    });
+  } else {
+    res.status(200).json({ message: "Bot çalışıyor!" });
+  }
 }
+
